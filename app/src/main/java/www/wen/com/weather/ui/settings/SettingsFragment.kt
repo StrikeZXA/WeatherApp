3207476f15407a14bb04fed3df41fab9a271f045
StrikeZXA <ZERO_AND_FREEDOM@126.com>
2020-09-28 21:53:53 +0800

    version: 0.1 主界面联动3个Fragment

---
+++ app/src/main/java/www/wen/com/weather/ui/settings/SettingsFragment.kt
@@ -0,0 +1,14 @@
+package www.wen.com.weather.ui.settings
+
+import androidx.fragment.app.Fragment
+
+/**
+ * @author WEN
+ * @Description:
+ * @date 2020/9/28 21:05
+ */
+class SettingsFragment : Fragment(){
+
+
+
+}
---
+++ app/src/main/java/www/wen/com/weather/ui/weather/current/CurrentWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package www.wen.com.weather.ui.weather.current
+
+import androidx.lifecycle.ViewModel
+
+class CurrentWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
