cb26d5cc827f563090448f4ece4abeab4c5bad8c
StrikeZXA <ZERO_AND_FREEDOM@126.com>
2020-10-12 10:46:03 +0800

    version：0.3 封装仓库数据缓存

--- app/src/main/java/www/wen/com/weather/ui/weather/current/CurrentWeatherFragment.kt
+++ app/src/main/java/www/wen/com/weather/ui/weather/current/CurrentWeatherFragment.kt
@@ -1,11 +1,11 @@
 package www.wen.com.weather.ui.weather.current
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import www.wen.com.weather.R
 
 class CurrentWeatherFragment : Fragment() {
@@ -25,6 +25,7 @@ class CurrentWeatherFragment : Fragment() {
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(CurrentWeatherViewModel::class.java)
         // TODO: Use the ViewModel
