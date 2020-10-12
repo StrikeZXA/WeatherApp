8bfb14e10350905c67bdb6684d55461fa5ce6aed
StrikeZXA <ZERO_AND_FREEDOM@126.com>
2020-10-12 10:55:27 +0800

    version：0.3 封装仓库数据缓存

--- app/src/main/java/www/wen/com/weather/ui/weather/current/CurrentWeatherFragment.kt
+++ app/src/main/java/www/wen/com/weather/ui/weather/current/CurrentWeatherFragment.kt
@@ -26,10 +26,6 @@ class CurrentWeatherFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        super.onActivityCreated(savedInstanceState)
-        super.onActivityCreated(savedInstanceState)
-        super.onActivityCreated(savedInstanceState)
-        super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(CurrentWeatherViewModel::class.java)
         // TODO: Use the ViewModel
     }
